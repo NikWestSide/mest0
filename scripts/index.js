d486a361cfabad3259ddef66139a73f02f50f676
NikWestSide <130581776+NikWestSide@users.noreply.github.com>
2023-07-26 19:33:17 +0700

    Add files via upload

--- scripts/index.js
+++ scripts/index.js
@@ -1,78 +1,36 @@
 const popupElement = document.querySelector('.popup');
-const popupEditButton = document.querySelector('.profile__info-button');
+const popupEditButton = document.querySelector('.profile__edit-button');
 const popupCloseButton = popupElement.querySelector('.popup__close');
-
-const initialCards = [
-  {
-    name: 'Карачаевск',
-    link: ''
-  },
-
-  {
-    name: 'Гора Эльбрус',
-    link: ''
-  },
-
-  {
-    name: 'Домбай',
-    link: ''
-  },
-
-  {
-    name: 'Гора Эльбрус',
-    link: ''
-  },
-
-  {
-    name: 'Карачаево-Черкессия',
-    link: ''
-  },
-
-  {
-    name: 'Алтай',
-    link: ''
-  },
-]
-
+const username = document.querySelector(".profile__title");
+const description = document.querySelector(".profile__subtitle");
+let nameInput = document.querySelector(".popup__input_type_name");
+let jobInput = document.querySelector(".popup__input_type_job");
 
 const openPopup = function () {
-  popupElement.classList.add('popup_opened');
-  nameInput.value = nameUser.textContent;
-  jobInput.value = jobUser.textContent;
+    popupElement.classList.add('popup_opened');
+    nameInput.value = username.textContent;
+    jobInput.value = description.textContent;
 }
 
 const closePopup = function () {
-  popupElement.classList.remove('popup_opened');
-}
+    popupElement.classList.remove('popup_opened');
+  }
+ 
+
+  popupEditButton.addEventListener('click', openPopup);
+
+  popupCloseButton.addEventListener('click', closePopup);
 
 
-const closePopupByClickOnOverlay = function(event) {
-  if (event.target === event.currentTarget)
-  closePopup();
-}
+let formElement = document.querySelector(".popup__form");
 
-
-popupEditButton.addEventListener('click', openPopup)
-
-
-popupCloseButton.addEventListener('click', closePopup);
-
-
-popupElement.addEventListener('click', closePopupByClickOnOverlay);
-
-
-const formElement = popupElement.querySelector('.popup__form');
-let nameInput = formElement.querySelector('.popup__input_type_name');
-let jobInput = formElement.querySelector('.popup__input_type_job');
-let nameUser = document.querySelector('.profile__info-name');
-let jobUser = document.querySelector('.profile__info-text');
-const formButton = popupElement.querySelector('.popup__form-btn');
 
 function handleFormSubmit (evt) {
-  evt.preventDefault();
-  nameUser.textContent = nameInput.value;
-  jobUser.textContent = jobInput.value;
-  closePopup();
+    evt.preventDefault();
+
+    username.textContent = nameInput.value;
+    description.textContent = jobInput.value;
+    closePopup();
 }
 
 formElement.addEventListener('submit', handleFormSubmit);
